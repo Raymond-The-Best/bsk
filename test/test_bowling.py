98d7928658b042dbda58e53276f6c3ec82fbbf9c
Raymond-The-Best <user.squire@gmail.com>
2024-10-28 17:14:48 +0200

    US1 - Tests 3 & 4

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -15,5 +15,34 @@ class TestBowlingGame(unittest.TestCase):
         self.game.add_frame(f)
         self.assertEquals(self.game.get_frame_at(0), f)
 
+
     def test_get_missing_frame_raises_BowlingError(self):
         self.assertRaises(BowlingError, self.game.get_frame_at, 0)
+
+    def test_game_created_10_frames(self):
+        self.game.add_frame(Frame(1,5))
+        self.game.add_frame(Frame(3,6))
+        self.game.add_frame(Frame(7,2))
+        self.game.add_frame(Frame(3,6))
+        self.game.add_frame(Frame(4, 4))
+        self.game.add_frame(Frame(5,3))
+        self.game.add_frame(Frame(3,3))
+        self.game.add_frame(Frame(4,5))
+        self.game.add_frame(Frame(8,1))
+        f = Frame(2,6)
+        self.game.add_frame(f)
+        self.assertEquals(self.game.get_frame_at(9), f)
+
+    def test_game_created_11_frames(self):
+     self.game.add_frame(Frame(1, 5))
+     self.game.add_frame(Frame(3, 6))
+     self.game.add_frame(Frame(7, 2))
+     self.game.add_frame(Frame(3, 6))
+     self.game.add_frame(Frame(4, 4))
+     self.game.add_frame(Frame(5, 3))
+     self.game.add_frame(Frame(3, 3))
+     self.game.add_frame(Frame(4, 5))
+     self.game.add_frame(Frame(8, 1))
+     self.game.add_frame(Frame(2, 6))
+
+     self.assertRaises(BowlingError, self.game.add_frame, Frame(1, 1))
